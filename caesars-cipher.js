5d9244b94fc8b7b53333fc2d67fbad4d31f470e6
BritishPandaChick <sbartley1080@gmail.com>
2018-11-25 19:28:21 -0500

    Add projects

---
+++ caesars-cipher.js
@@ -0,0 +1,16 @@
+function rot13(str) { //LBH QVQ VG!
+  return str.split('')
+
+  .map.call(str, function(char){
+    let x = char.charCodeAt(0);
+    if(x < 65 || x > 90) {
+      return String.fromCharCode(x);
+    } else if (x < 78) {
+      return String.fromCharCode(x + 13);
+    }
+    return String.fromCharCode(x - 13);
+  }).join('');
+}
+
+
+rot13("SERR PBOR PNZC");
